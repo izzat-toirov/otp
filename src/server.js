cd54a6ae22a9fa8cc951b8617d02f7f5ea6fb42f
Izzat <tdmkenzie@gmail.com>
2025-05-02 07:20:21 +0300

    OTP

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import userRouter from "./routers/user.js";
 import authRouter from "./routers/auth.js";
 import { swaggerSpec, swaggerUi } from "./docs/swagger.js";
+import logger from "./utils/logger/logger.js"
 
 
 config();
@@ -17,4 +18,24 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/users', userRouter);
 app.use('/', authRouter);
 
-app.listen(PORT, ()=> console.log(`Server started ${PORT}`));
+process.on('uncaughtException', (err) => {
+    if (err) console.log(`Uncaught expetion: ${err}`);
+    process.exit(1);
+  });
+  
+  process.on('unhandledRejection', (reasion, promise) => {
+    console.log(`Unhandled rejection: ${reasion}`);
+  });
+  
+  app.use((err, req, res, next) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ eror: err.message || 'Internal server error' });
+    } else {
+      next();
+    }
+  });
+  
+  
+  app.listen(PORT, logger.info(`Server started ${PORT}`));
